baaacc62219a6692797f6169033d3747d7d93a48
BeltranRafael3D <rafaelbeltrananimacion@gmail.com>
2019-11-14 14:25:52 +0100

    RBT-CLASE141119
    RBT-CLASE141119

---
+++ Assets/BeltranRafaelCUADERNO.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeltranRafaelCUADERNO : MonoBehaviour
+{
+
+    /* Para escribir notas 
+     -float almacena datos y input es el q pregunta que esta pasando
+     
+     rb.AddForce(movimiento*5); esto hace multiplicar la velocidad por 5 
+     (esto por programacion)
+     
+     public float velocidad (casilla velocidad)
+     
+     */
+
+
+}
